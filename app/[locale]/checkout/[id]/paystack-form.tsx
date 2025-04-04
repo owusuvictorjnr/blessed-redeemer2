af2cbcc92f758b8bdf781bd93c9a5ce3f2d6cc97
owusuvictorjnr <owusuvictor80@gmail.com>
2025-04-04 11:46:26 +0000

    remove unused codes

--- app/[locale]/checkout/[id]/paystack-form.tsx
+++ app/[locale]/checkout/[id]/paystack-form.tsx
@@ -1,24 +1,25 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { IOrder } from '@/lib/db/models/order.model';
-import { useRouter } from 'next/navigation';
-// import PaystackPop from '@paystack/inline-js'
+'use client'
+import { Button } from '@/components/ui/button'
+import { IOrder } from '@/lib/db/models/order.model'
+import { useRouter } from 'next/navigation'
 
-
-export default function PaystackForm({ order, paystackUrl }: { order: IOrder; paystackUrl: string | null }) {
-  const router = useRouter();
-
+export default function PaystackForm({
+  paystackUrl,
+}: {
+  order: IOrder
+  paystackUrl: string | null
+}) {
   return (
     <div>
       <div className='text-xl'>Paystack Checkout</div>
       <Button
         className='w-full'
         size='lg'
-        onClick={() => window.location.href = paystackUrl || '#'}
+        onClick={() => (window.location.href = paystackUrl || '#')}
         disabled={!paystackUrl}
       >
         Proceed to Paystack
       </Button>
     </div>
-  );
+  )
 }
