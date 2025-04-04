fed6b5929049a1205d697fdc337c7a3f28db9a37
owusuvictorjnr <owusuvictor80@gmail.com>
2025-04-04 10:57:42 +0000

    paystack-payment-success page

---
+++ app/[locale]/checkout/[id]/paystack-payment-success/page.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link'
+import { notFound, redirect } from 'next/navigation'
+
+import { Button } from '@/components/ui/button'
+import { getOrderById } from '@/lib/actions/order.actions'
+
+export default async function SuccessPage(props: {
+  params: { id: string }
+  searchParams: { reference: string }
+}) {
+  const { id } = props.params
+  const { reference } = props.searchParams
+
+  const order = await getOrderById(id)
+  if (!order) notFound()
+
+  // Verify Paystack Payment
+  const response = await fetch(
+    `https://api.paystack.co/transaction/verify/${reference}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+      },
+    }
+  )
+  const data = await response.json()
+
+  const isSuccess = data?.data?.status === 'success'
+
+  if (!isSuccess) return redirect(`/checkout/${id}`)
+  if (order.isPaid) return redirect(`/checkout/${id}`)
+  if (order.paymentResult?.status === 'success')
+    return redirect(`/checkout/${id}`)
+
+  return (
+    <div className='max-w-4xl w-full mx-auto space-y-8'>
+      <div className='flex flex-col gap-6 items-center '>
+        <h1 className='font-bold text-2xl lg:text-3xl'>
+          Thanks for your purchase
+        </h1>
+        <div>We are now processing your order.</div>
+        <Button asChild>
+          <Link href={`/account/orders/${id}`}>View order</Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
