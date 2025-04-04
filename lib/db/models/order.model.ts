740f9a688f92a7dcd60289eaf9cce7df949b11b4
owusuvictorjnr <owusuvictor80@gmail.com>
2025-04-04 11:47:13 +0000

    repalce any eith string

--- lib/db/models/order.model.ts
+++ lib/db/models/order.model.ts
@@ -2,7 +2,7 @@ import { IOrderInput } from '@/types'
 import { Document, Model, model, models, Schema } from 'mongoose'
 
 export interface IOrder extends Document, IOrderInput {
-  userEmail: any
+  userEmail: string
   _id: string
   createdAt: Date
   updatedAt: Date
